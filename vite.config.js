fae28a585f7d869753638089567a3e17e07c7507
Mdiskbbot <158266051+Mdiskbbot@users.noreply.github.com>
2026-09-13 12:27:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // set base to "/<repo>/". If deploying to Vercel/Netlify, leave it as "/".
 export default defineConfig({
   plugins: [react()],
-  base: "/attendance-platform/",
+  base: "/",
 });
